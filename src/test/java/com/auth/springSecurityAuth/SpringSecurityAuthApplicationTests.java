adc817d374094daded48b18aad0c07d53d145f2c
bodofia olinga <bodifolinga@gmail.com>
2025-07-24 16:00:39 +0200

    mise en place des roles et creation des utilisateurs

---
+++ src/test/java/com/auth/springSecurityAuth/SpringSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.springSecurityAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
