041ea21b92f555b522eb73c080bad6ae53837713
bodofia olinga <bodifolinga@gmail.com>
2025-07-26 13:49:49 +0200

    configuration de oauth2

--- src/main/java/com/auth/springSecurityAuth/model/DBUser.java
+++ src/main/java/com/auth/springSecurityAuth/model/DBUser.java
@@ -3,15 +3,17 @@ package com.auth.springSecurityAuth.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Data
 public class DBUser {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String username;
     private String password;
     private String role;
